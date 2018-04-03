3f65b483592e28acf8399213d47ffec776096988
RobertDober <robert.dober@gmail.com>
2018-04-03 18:44:36 +0200

    Refs: #171; Typespecs for Earmark.Helpers

--- lib/earmark/helpers.ex
+++ lib/earmark/helpers.ex
@@ -3,10 +3,12 @@ defmodule Earmark.Helpers do
   @doc """
   Expand tabs to multiples of 4 columns
   """
+  @spec expand_tabs( String.t ) :: String.t
   def expand_tabs(line) do
     Regex.replace(~r{(.*?)\t}, line, &expander/2)
   end
 
+  @spec expander( any, String.t ) :: String.t
   defp expander(_, leader) do
     extra = 4 - rem(String.length(leader), 4)
     leader <> pad(extra)
@@ -15,10 +17,12 @@ defmodule Earmark.Helpers do
   @doc """
   Remove newlines at end of line
   """
+  @spec remove_line_ending( String.t ) :: String.t
   def remove_line_ending(line) do
     line |> String.trim_trailing("\n") |> String.trim_trailing("\r")
   end
 
+  @spec pad( pos_integer ) :: String.t
   defp pad(1), do: " "
   defp pad(2), do: "  "
   defp pad(3), do: "   "
@@ -28,6 +32,7 @@ defmodule Earmark.Helpers do
   `Regex.replace` with the arguments in the correct order
   """
 
+  @spec replace( String.t, Regex.t, String.t, Keyword.t ) :: String.t
   def replace(text, regex, replacement, options \\ []) do
     Regex.replace(regex, text, replacement, options)
   end
@@ -38,6 +43,7 @@ defmodule Earmark.Helpers do
   Percent-escapes a URI, and after that escapes any
   `&`, `<`, `>`, `"`, `'`.
   """
+  @spec encode( String.t ) :: String.t
   def encode(html) do
     URI.encode(html) |> escape(true)
   end
@@ -48,6 +54,7 @@ defmodule Earmark.Helpers do
    convert non-entity ampersands.
   """
 
+  @spec escape( String.t, boolean ) :: String.t
   def escape(html, encode \\ false)
 
   def escape(html, false), do: _escape(Regex.replace(~r{&(?!#?\w+;)}, html, "&amp;"))
